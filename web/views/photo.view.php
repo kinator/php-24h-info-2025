dc6e81909af2449e9263bdb35afbba75104f5f79
kinator <behani.julien@gmx.fr>
2025-05-23 23:17:25 +0000

    added content to photo page

--- web/views/photo.view.php
+++ web/views/photo.view.php
@@ -1,11 +1,16 @@
 <?php
 include "$root/inc/head.php";
 ?>
-
-<div class="margin w3-border w3-padding" style="background: white;">
-  <h1>Photo</h1>
+<div style='background: black;'>
+  <div class="margin w3-padding w3-center">
+    <div class="w3-padding-32 w3-grey w3-circle w3-center" style="width: 230px; margin: auto;">
+      <h1>Votre souris illuminera la voie</h1>
+    </div>
+    <img src="/img/logo.png" style="width:50vw"/>
+  </div>
 </div>
 
 <?php
+$changeBackground = 'black';
 include "$root/inc/footer.php";
 ?>
